438c2f74ba69a26cad09e85195e43e484204dfa1
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-22 22:57:43 +0700

    Update

---
+++ src/app/author/[id]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AuthorDetail() {
+  return (
+    <div>
+      AuthorDetail 
+    </div>
+  )
+}
---
+++ src/app/author/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Authors() {
+  return (
+    <div>
+      List of Authors (4)
+    </div>
+  )
+}
