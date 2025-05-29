2ff9c50a02727508908fdfb86692535bd9093003
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-29 22:19:55 +0700

    Update

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import Link from 'next/link'
-import {Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger} from '../../../src/components/ui/menubar'
+import React from "react";
+import Link from "next/link";
+import {Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger} from "../../../src/components/ui/menubar";
 
 const Header = () => {
   return (
     <div>
       <header className="flex flex-row justify-between items-center bg-indigo-800 my-5 mx-5 lg:mx-33 px-2 py-4 rounded-full">
         <Link href="/">
-        <h1 className="text-2xl lg:text-3xl font-bold ms-3">Bloggy</h1>
+          <h1 className="text-2xl lg:text-3xl font-bold ms-3">Bloggy</h1>
         </Link>
         <Menubar>
           <MenubarMenu>
-            <MenubarTrigger>Home</MenubarTrigger>
-            </MenubarMenu>       
+            <MenubarTrigger>
+              <Link href="/">Home</Link>
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Link href="/categories">Categories</Link>
+            </MenubarTrigger>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger>Dashboard</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem>
+                <Link className="/dashboard">Overview</Link>
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
         </Menubar>
-        </header>
-        
+      </header>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
       <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"/>
       </head>
-      <body className={`${sora?.className} antialiased`}>{children}</body>
+      <body className={`${sora?.className} antialiased bg-gradient-to-b from-[#050611] to-[#04000c] text-white relative`}>{children}</body>
     </html>
   );
 }
